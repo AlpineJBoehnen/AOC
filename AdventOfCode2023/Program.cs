35ef542191c5b01ef1c48653d223ce57abf7e9d9
Justin Boehnen <jboehnen@alpine-lis.com>
2023-12-01 19:33:39 -0800

    day 2 part 2

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -4,13 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] input = ReadInput();
+        string[] input = ReadInput(@"C:\Users\Justin Boehnen\Desktop\input.txt");
         Stopwatch sw = Stopwatch.StartNew();
 
-        string output = Day1_Part1(input);
+        string output = Day1_Part2(input);
 
         sw.Stop();
         Console.WriteLine($"Time taken: {sw.ElapsedMilliseconds}ms");
+        Console.WriteLine($"Output: {output}");
         WriteStringToFileOnDesktop("output.txt", output);
     }
 
@@ -22,12 +23,12 @@ internal class Program
         };
 
         int total = 0;
-        foreach(string line in input)
+        foreach (string line in input)
         {
             char? first = line.FirstOrDefault(_ => isDigit(_));
             char? last = line.LastOrDefault(_ => isDigit(_));
 
-            if(!first.HasValue || !last.HasValue)
+            if (!first.HasValue || !last.HasValue)
             {
                 continue;
             }
@@ -38,15 +39,82 @@ internal class Program
         return total.ToString();
     }
 
+    private static string Day1_Part2(string[] input)
+    {
+        Dictionary<string, string> numbers = new()
+        {
+            {"1", "1"},
+            {"one", "1"},
+            {"2", "2"},
+            {"two", "2"},
+            {"3", "3"},
+            {"three", "3"},
+            {"4", "4"},
+            {"four", "4"},
+            {"5", "5"},
+            {"five", "5"},
+            {"6", "6"},
+            {"six", "6"},
+            {"7", "7"},
+            {"seven", "7"},
+            {"8", "8"},
+            {"eight", "8"},
+            {"9", "9"},
+            {"nine", "9"}
+        };
+
+        int total = 0;
+
+        foreach (string line in input)
+        {
+            int lowest = int.MaxValue;
+            string? lowestKey = null;
+            int highest = -1;
+            string? highestKey = null;
+
+            foreach (string key in numbers.Keys)
+            {
+                int firstInd = line.IndexOf(key);
+
+                if (firstInd != -1 && firstInd < lowest)
+                {
+                    lowest = firstInd;
+                    lowestKey = key;
+                }
+
+                int lastInd = line.LastIndexOf(key);
+
+                if (lastInd != -1 && lastInd > highest)
+                {
+                    highest = lastInd;
+                    highestKey = key;
+                }
+            }
+            if (lowestKey != null && highestKey != null)
+            {
+                total += int.Parse($"{numbers[lowestKey]}{numbers[highestKey]}");
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        return total.ToString();
+    }
     private static string[] ReadInput()
     {
-        Console.WriteLine("Enter the path to the text file:");
         string? filePath;
-        while(string.IsNullOrWhiteSpace(filePath = Console.ReadLine()))
+        while (string.IsNullOrWhiteSpace(filePath = Console.ReadLine()))
         {
             Console.WriteLine("Enter the path to the text file:");
         }
 
+        return File.ReadAllLines(filePath);
+    }
+
+    private static string[] ReadInput(string filePath)
+    {
         return File.ReadAllLines(filePath);
     }
 
