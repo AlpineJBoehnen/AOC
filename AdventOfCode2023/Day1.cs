5be156982f01c9bfef3962e988f7936b4c0155fb
Justin Boehnen <jboehnen@alpine-lis.com>
2023-12-03 22:45:45 -0800

    Refactor

---
+++ AdventOfCode2023/Day1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day1 : ISolution
+    {
+        public static string Part1(string[] input)
+        {
+            var isDigit = (char c) =>
+            {
+                return int.TryParse(c.ToString(), out _);
+            };
+
+            int total = 0;
+            foreach (string line in input)
+            {
+                char? first = line.FirstOrDefault(_ => isDigit(_));
+                char? last = line.LastOrDefault(_ => isDigit(_));
+
+                if (!first.HasValue || !last.HasValue)
+                {
+                    continue;
+                }
+
+                total += int.Parse($"{first}{last}");
+            }
+
+            return total.ToString();
+        }
+
+        public static string Part2(string[] input)
+        {
+            Dictionary<string, string> numbers = new()
+        {
+            {"1", "1"},
+            {"one", "1"},
+            {"2", "2"},
+            {"two", "2"},
+            {"3", "3"},
+            {"three", "3"},
+            {"4", "4"},
+            {"four", "4"},
+            {"5", "5"},
+            {"five", "5"},
+            {"6", "6"},
+            {"six", "6"},
+            {"7", "7"},
+            {"seven", "7"},
+            {"8", "8"},
+            {"eight", "8"},
+            {"9", "9"},
+            {"nine", "9"}
+        };
+
+            int total = 0;
+
+            foreach (string line in input)
+            {
+                int lowest = int.MaxValue;
+                string? lowestKey = null;
+                int highest = -1;
+                string? highestKey = null;
+
+                foreach (string key in numbers.Keys)
+                {
+                    int firstInd = line.IndexOf(key);
+
+                    if (firstInd != -1 && firstInd < lowest)
+                    {
+                        lowest = firstInd;
+                        lowestKey = key;
+                    }
+
+                    int lastInd = line.LastIndexOf(key);
+
+                    if (lastInd != -1 && lastInd > highest)
+                    {
+                        highest = lastInd;
+                        highestKey = key;
+                    }
+                }
+                if (lowestKey != null && highestKey != null)
+                {
+                    total += int.Parse($"{numbers[lowestKey]}{numbers[highestKey]}");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+
+            return total.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2023/Day2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day2 : ISolution
+    {
+        public static string Part1(string[] input)
+        {
+            int[] limits = [12, 13, 14]; //R G B
+            int total = 0;
+
+            foreach (string line in input)
+            {
+                int[] colorCounts = new int[3]; //R G B
+                var parts = line.Split(',', ':', ';');
+                int roundId = int.Parse(parts[0].Replace("Game ", ""));
+
+                foreach (string part in parts.Skip(1))
+                {
+                    int count = int.Parse(part.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]);
+                    string colorName = part.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+                    int colorIndex = colorName == "red" ? 0 : colorName == "green" ? 1 : 2;
+
+                    if (count > colorCounts[colorIndex])
+                    {
+                        colorCounts[colorIndex] = count;
+                    }
+                }
+
+                if (colorCounts[0] <= limits[0] && colorCounts[1] <= limits[1] && colorCounts[2] <= limits[2])
+                {
+                    total += roundId;
+                }
+            }
+
+            return total.ToString();
+        }
+
+        public static string Part2(string[] input)
+        {
+            int total = 0;
+
+            foreach (string line in input)
+            {
+                int[] colorCounts = new int[3]; //R G B
+                var parts = line.Split(',', ':', ';');
+                int roundId = int.Parse(parts[0].Replace("Game ", ""));
+
+                foreach (string part in parts.Skip(1))
+                {
+                    int count = int.Parse(part.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]);
+                    string colorName = part.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+                    int colorIndex = colorName == "red" ? 0 : colorName == "green" ? 1 : 2;
+
+                    if (count > colorCounts[colorIndex])
+                    {
+                        colorCounts[colorIndex] = count;
+                    }
+                }
+
+                total += (colorCounts[0] * colorCounts[1] * colorCounts[2]);
+            }
+
+            return total.ToString();
+        }
+    }
+}
