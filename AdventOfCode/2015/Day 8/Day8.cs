5ae050f1e0ca3a003acdad7d75261ff7bcea3b88
AlpineJBoehnen <jboehnen@alpine-lis.com>
2023-12-11 15:59:24 -0800

    day 8 prog

--- AdventOfCode/2015/Day 8/Day8.cs
+++ AdventOfCode/2015/Day 8/Day8.cs
@@ -28,7 +28,21 @@ public partial class Day8 : AdventOfCodeDay
 
     protected override string SolvePart2(string[] input)
     {
-        return "";
+        int total = 0;
+        foreach (string line in input)
+        {
+            string copy = line;
+            copy = copy.Replace("\\\\", "\\\\\\\\");    // \\ -> \\\\
+            copy = copy.Replace("\\\"", "\\\\\"");      // \" -> \\\"
+            copy = copy.Replace("\"", "\\\"");          // " -> \"
+            copy = $"\"{copy}\"";
+            foreach (Match match in asciiRegex().Matches(copy))
+            {
+                copy = copy.Replace(match.Value, "\\xxx");
+            }
+            total += copy.Length - line.Length;
+        }
+        return total.ToString();
     }
 
     [GeneratedRegex(@"\\x[0-f]{2}")]
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,15 +1,15 @@
 ﻿using AdventOfCode;
 
 // Select Year
-//using AdventOfCode.Y2015;
-using AdventOfCode.Y2023;
+using AdventOfCode.Y2015;
+//using AdventOfCode.Y2023;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         // Select Day
-        AdventOfCodeDay day = new Day11();
+        AdventOfCodeDay day = new Day8();
 
         day.Run(false);
     }
