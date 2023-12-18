6e0aa4d5f708384684d0b1be4bbe04fae1ffce13
AlpineJBoehnen <jboehnen@alpine-lis.com>
2023-12-18 13:01:27 -0800

    day 18 progress

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,8 +9,8 @@ internal class Program
     private static void Main(string[] args)
     {
         // Select Day
-        AdventOfCodeDay day = new Day15();
+        AdventOfCodeDay day = new Day18();
 
-        day.Run(false);
+        day.Run(true);
     }
 }
