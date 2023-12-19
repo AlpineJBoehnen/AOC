fcc9ad89772bfaae7cbf384c9cff13a740edca39
Justin Boehnen <jboehnen@alpine-lis.com>
2023-12-18 21:07:14 -0800

    stuff

--- AdventOfCode/2023/Day 16/Day16.cs
+++ AdventOfCode/2023/Day 16/Day16.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Specialized;
-using System.Globalization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace AdventOfCode.Y2023;
 
@@ -11,11 +12,326 @@ public class Day16 : AdventOfCodeDay
 
     protected override string SolvePart1(string[] input)
     {
-        return "";
+        bool[][] energyMap = new bool[input.Length][];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            energyMap[i] = new bool[input[i].Length];
+        }
+
+        int energy = CountEnergy(input.Select(x => x.ToCharArray()).ToArray(), new Beam(0, 0, _right));
+
+        return energy.ToString();
     }
 
     protected override string SolvePart2(string[] input)
     {
-        return "";
+        int maxEnergized = 0;
+
+        // top
+        for (int x = 0; x < input[0].Length; x++)
+        {
+            int energy = CountEnergy(input.Select(x => x.ToCharArray()).ToArray(), new Beam(x, 0, _down));
+
+            if (energy > maxEnergized)
+            {
+                maxEnergized = energy;
+            }
+        }
+
+        // bottom
+        for (int x = 0; x < input[0].Length; x++)
+        {
+            int energy = CountEnergy(input.Select(x => x.ToCharArray()).ToArray(), new Beam(x, input.Length - 1, _up));
+
+            if (energy > maxEnergized)
+            {
+                maxEnergized = energy;
+            }
+        }
+
+        // left
+        for (int y = 0; y < input.Length; y++)
+        {
+            int energy = CountEnergy(input.Select(x => x.ToCharArray()).ToArray(), new Beam(0, y, _right));
+
+            if (energy > maxEnergized)
+            {
+                maxEnergized = energy;
+            }
+        }
+
+        // right
+        for (int y = 0; y < input.Length; y++)
+        {
+            int energy = CountEnergy(input.Select(x => x.ToCharArray()).ToArray(), new Beam(input[0].Length - 1, y, _left));
+
+            if (energy > maxEnergized)
+            {
+                maxEnergized = energy;
+            }
+        }
+
+        return maxEnergized.ToString();
+    }
+
+    private const byte _up = 0b00001000;
+    private const byte _down = 0b00000100;
+    private const byte _left = 0b00000010;
+    private const byte _right = 0b00000001;
+    private readonly static HashSet<Beam> _b = [];
+    private static Beam? GetBeam(char[][] map, Beam b, List<Beam>? visited = null)
+    {
+        // base case
+        if (b.X < 0 || b.Y < 0 || b.X == map[0].Length || b.Y == map.Length)
+        {
+            return null;
+        }
+
+        // already processed
+        if (_b.TryGetValue(b, out Beam value))
+        {
+            return value;
+        }
+
+        // loop detection
+        if (visited == null)
+        {
+            visited = [];
+        }
+        else if (visited.Contains(b))
+        {
+            int bInd = visited.IndexOf(b);
+            b.IsLoop = true;
+            b.Children.UnionWith(visited[(bInd+1)..]);
+            return b;
+        }
+
+        visited.Add(b);
+        Beam? c1 = null;
+        Beam? c2 = null;
+
+        if (b.IsMovingUp)
+        {
+            switch (map[b.Y][b.X])
+            {
+                case '.':
+                case '|':
+                    c1 = GetBeam(map, b.Up(), new(visited));
+                    break;
+                case '/':
+                    c1 = GetBeam(map, b.Right(), new(visited));
+                    break;
+                case '\\':
+                    c1 = GetBeam(map, b.Left(), new(visited));
+                    break;
+                case '-':
+                    c2 = GetBeam(map, b.Right(), new(visited));
+                    c1 = GetBeam(map, b.Left(), new(visited));
+                    break;
+            }
+        }
+        else if (b.IsMovingDown)
+        {
+            switch (map[b.Y][b.X])
+            {
+                case '.':
+                case '|':
+                    c1 = GetBeam(map, b.Down(), new(visited));
+                    break;
+                case '/':
+                    c1 = GetBeam(map, b.Left(), new(visited));
+                    break;
+                case '\\':
+                    c1 = GetBeam(map, b.Right(), new(visited));
+                    break;
+                case '-':
+                    c1 = GetBeam(map, b.Left(), new(visited));
+                    c2 = GetBeam(map, b.Right(), new(visited));
+                    break;
+            }
+        }
+        else if (b.IsMovingLeft)
+        {
+            switch (map[b.Y][b.X])
+            {
+                case '.':
+                case '-':
+                    c1 = GetBeam(map, b.Left(), new(visited));
+                    break;
+                case '/':
+                    c1 = GetBeam(map, b.Down(), new(visited));
+                    break;
+                case '\\':
+                    c1 = GetBeam(map, b.Up(), new(visited));
+                    break;
+                case '|':
+                    c1 = GetBeam(map, b.Down(), new(visited));
+                    c2 = GetBeam(map, b.Up(), new(visited));
+                    break;
+            }
+        }
+        else // Right
+        {
+            switch (map[b.Y][b.X])
+            {
+                case '.':
+                case '-':
+                    c1 = GetBeam(map, b.Right(), new(visited));
+                    break;
+                case '/':
+                    c1 = GetBeam(map, b.Up(), new(visited));
+                    break;
+                case '\\':
+                    c1 = GetBeam(map, b.Down(), new(visited));
+                    break;
+                case '|':
+                    c1 = GetBeam(map, b.Up(), new(visited));
+                    c2 = GetBeam(map, b.Down(), new(visited));
+                    break;
+            }
+        }
+
+        if(c1.HasValue)
+        {
+            b.Children.Add(c1.Value);
+        }
+        if(c2.HasValue)
+        {
+            b.Children.Add(c2.Value);
+        }
+
+        _b.Add(b);
+        return b;
+    }
+
+    private static int CountEnergy(char[][] map, Beam b)
+    {
+        var beam = GetBeam(map, b) ?? throw new Exception("Bad start");
+        var distinctCoords = beam.Path().Select(x => (x.X, x.Y)).Distinct().ToHashSet();
+        var count = distinctCoords.Count();
+        return count;
+    }
+
+    private struct Beam
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public byte Vec { get; set; }
+        public HashSet<Beam> Children { get; set; } = [];
+        public bool IsLoop { get; set; } = false;
+        public Beam(int x, int y, byte directionVector)
+        {
+            X = x;
+            Y = y;
+            Vec = directionVector;
+        }
+
+        public readonly HashSet<Beam> Path()
+        {
+            HashSet<Beam> path = [this];
+            if (IsLoop)
+            {
+                path.UnionWith(Children);
+            }
+            else
+            {
+                foreach(var child in Children)
+                {
+                    path.UnionWith(child.Path());
+                }
+            }
+
+            return path;
+        }
+
+        public Beam Up()
+        {
+            return new Beam(X, Y - 1, _up);
+        }
+
+        public Beam Down()
+        {
+            return new Beam(X, Y + 1, _down);
+        }
+
+        public Beam Left()
+        {
+            return new Beam(X - 1, Y, _left);
+        }
+
+        public Beam Right()
+        {
+            return new Beam(X + 1, Y, _right);
+        }
+
+        public readonly bool IsMovingUp => (Vec & _up) != 0;
+
+        public readonly bool IsMovingDown => (Vec & _down) != 0;
+
+        public readonly bool IsMovingLeft => (Vec & _left) != 0;
+
+        public readonly bool IsMovingRight => (Vec & _right) != 0;
+
+        public override readonly string ToString()
+        {
+            string s = $"({X}, {Y})";
+            if (IsMovingUp)
+            {
+                s += " U";
+            }
+            else if (IsMovingDown)
+            {
+                s += " D";
+            }
+            else if (IsMovingLeft)
+            {
+                s += " L";
+            }
+            else
+            {
+                s += " R";
+            }
+
+            s += $" [{Children.Count}]";
+
+            return s;
+        }
+
+        int _yMask = 0b0000_0000_0000_0000_0011_1111_1111_1111;
+
+        // XXXX XXXX XXXX XXYY YYYY YYYY YYYY VVVV
+        public override int GetHashCode()
+        {
+            int hash = X << 18 ^ (Y & _yMask) << 4 ^ Vec;
+            return hash;
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj is not Beam b)
+            {
+                return false;
+            }
+
+            if(X != b.X)
+            {
+                return false;
+            }
+
+            if(Y != b.Y)
+            {
+                return false;
+            }
+
+            if(Vec != b.Vec)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
--- AdventOfCode/2023/Day 17/Day17.cs
+++ AdventOfCode/2023/Day 17/Day17.cs
@@ -1,21 +1,165 @@
-﻿using System.Collections.Specialized;
-using System.Globalization;
+﻿//using System.Collections.Specialized;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
 
-namespace AdventOfCode.Y2023;
+//namespace AdventOfCode.Y2023;
 
-public class Day17 : AdventOfCodeDay
-{
-    public Day17() : base(2023, 17)
-    {
-    }
+//public class Day17 : AdventOfCodeDay
+//{
+//    /*
+//function reconstruct_path(cameFrom, current)
+//    total_path := {current}
+//    while current in cameFrom.Keys:
+//        current := cameFrom[current]
+//        total_path.prepend(current)
+//    return total_path
 
-    protected override string SolvePart1(string[] input)
-    {
-        return "";
-    }
+//// A* finds a path from start to goal.
+//// h is the heuristic function. h(n) estimates the cost to reach goal from node n.
+//function A_Star(start, goal, h)
+//    // The set of discovered nodes that may need to be (re-)expanded.
+//    // Initially, only the start node is known.
+//    // This is usually implemented as a min-heap or priority queue rather than a hash-set.
+//    openSet := {start}
 
-    protected override string SolvePart2(string[] input)
-    {
-        return "";
-    }
-}
+//    // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from the start
+//    // to n currently known.
+//    cameFrom := an empty map
+
+//    // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
+//    gScore := map with default value of Infinity
+//    gScore[start] := 0
+
+//    // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
+//    // how cheap a path could be from start to finish if it goes through n.
+//    fScore := map with default value of Infinity
+//    fScore[start] := h(start)
+
+//    while openSet is not empty
+//        // This operation can occur in O(Log(N)) time if openSet is a min-heap or a priority queue
+//        current := the node in openSet having the lowest fScore[] value
+//        if current = goal
+//            return reconstruct_path(cameFrom, current)
+
+//        openSet.Remove(current)
+//        for each neighbor of current
+//            // d(current,neighbor) is the weight of the edge from current to neighbor
+//            // tentative_gScore is the distance from start to the neighbor through current
+//            tentative_gScore := gScore[current] + d(current, neighbor)
+//            if tentative_gScore < gScore[neighbor]
+//                // This path to neighbor is better than any previous one. Record it!
+//                cameFrom[neighbor] := current
+//                gScore[neighbor] := tentative_gScore
+//                fScore[neighbor] := tentative_gScore + h(neighbor)
+//                if neighbor not in openSet
+//                    openSet.add(neighbor)
+
+//    // Open set is empty but goal was never reached
+//    return failure
+//     */
+
+//    public Day17() : base(2023, 17)
+//    {
+//    }
+
+//    protected override string SolvePart1(string[] input)
+//    {
+//        return "";
+//    }
+
+//    protected override string SolvePart2(string[] input)
+//    {
+//        return "";
+//    }
+
+//    private static (Node start, Node end) GetGraph(string[] input)
+//    {
+//        Dictionary<(ushort x, ushort y), Node> nodes = new();
+//        Node? start = null;
+//        Node? end = null;
+//        for (ushort yy = 0; yy < input.Length; yy++)
+//        {
+//            for (ushort xx = 0; xx < input[0].Length; xx++)
+//            {
+//                Node node = new(xx, yy, int.Parse(input[yy][xx].ToString()));
+//                nodes.Add((xx, yy), node);
+
+//                if ((xx, yy) == (0, 0))
+//                {
+//                    start = node;
+//                }
+//                else if ((xx, yy) == (input[0].Length - 1, input.Length - 1))
+//                {
+//                    end = node;
+//                }
+//            }
+//        }
+
+//        foreach (var node in nodes)
+//        {
+//            if (node.Key.x > 0) 
+//            {
+//                node.Value.Neighbors.Enqueue(nodes[(node.X - 1, node.Y)], node.Value);
+//            }
+//            if (node.Key.x < input[0].Length - 1)
+//            {
+//                node.Value.Neighbors.Enqueue(nodes[(node.Key.x + 1, node.Key.y)], 1);
+//            }
+//            if (node.Key.y > 0)
+//            {
+//                node.Value.Neighbors.Enqueue(nodes[(node.Key.x, node.Key.y - 1)], 1);
+//            }
+//            if (node.Key.y < input.Length - 1)
+//            {
+//                node.Value.Neighbors.Enqueue(nodes[(node.Key.x, node.Key.y + 1)], 1);
+//            }
+//        }
+
+//        if (start == null || end == null)
+//        {
+//            throw new Exception("Start or end node not found");
+//        }
+
+//        return (start.Value, end.Value);
+//    }
+
+//    private static int AStar(Node start, Node end, Func<Node, int[][]> heuristic)
+//    {
+//        PriorityQueue<Node, int> openSet = new();
+//        openSet.Enqueue(start, 0);
+
+//        List<Node> cameFrom = [];
+
+//    }
+
+//    private struct Node
+//    {
+//        public ushort X { get; set; }
+//        public ushort Y { get; set; }
+//        public int Value { get; set; }
+//        public int GScore { get; set; } = int.MaxValue;
+//        public PriorityQueue<Node, int> Neighbors { get; set; } = new();
+
+//        public Node(ushort x, ushort y, int value)
+//        {
+//            X = x;
+//            Y = y;
+//            Value = value;
+//        }
+
+//        public override int GetHashCode()
+//        {
+//            return X << 16 | Y;
+//        }
+
+//        public override bool Equals([NotNullWhen(true)] object? obj)
+//        {
+//            if (obj is Node node)
+//            {
+//                return (node.X, node.Y) == (X, Y);
+//            }
+
+//            return false;
+//        }
+//    }
+//}
