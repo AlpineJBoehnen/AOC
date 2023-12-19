e59b9730f13dedc01eb7e37cd51e2b623a306a82
Justin Boehnen <jboehnen@alpine-lis.com>
2023-12-18 23:14:26 -0800

    day 18

--- AdventOfCode/2023/Day 18/Day18.cs
+++ AdventOfCode/2023/Day 18/Day18.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Specialized;
 using System.Drawing;
 using System.Globalization;
+using System.Runtime.ExceptionServices;
 
 namespace AdventOfCode.Y2023;
 
@@ -47,46 +48,109 @@ public class Day18 : AdventOfCodeDay
         int offsetX = -cells.Min(_ => _.X);
         int offsetY = -cells.Min(_ => _.Y);
 
-        int[,] paint = new int[height, width];
-        bool[,] upsAndDowns = new bool[height, width];
+        int[][] paint = new int[height][];
+        bool[][] upsAndDowns = new bool[height][];
 
         foreach (PaintedCell cell in cells)
         {
-            paint[cell.Y + offsetY, cell.X + offsetX] = cell.Color;
-            upsAndDowns[cell.Y + offsetY, cell.X + offsetX] = cell.IsUpOrDown;
-        }
-
-        for (int yy = 0; yy < height; yy++)
-        {
-            for (int xx = 0; xx < width; xx++)
+            if (paint[cell.Y + offsetY] == null)
             {
-                Console.Write(paint[yy, xx] == 0 ? '.' : '#');
+                paint[cell.Y + offsetY] = new int[width];
+                upsAndDowns[cell.Y + offsetY] = new bool[width];
             }
-            Console.Write('\n');
+
+            paint[cell.Y + offsetY][cell.X + offsetX] = cell.Color;
+            upsAndDowns[cell.Y + offsetY][cell.X + offsetX] = cell.IsUpOrDown;
         }
 
+        (int Y, int X) firstInside = (0, 0);
+        for (int xx = 0; xx < width; xx++)
+        {
+            if (paint[0][xx] != 0)
+            {
+                firstInside = (1, xx + 1);
+                break;
+            }
+        }
+
+        FloodFill(ref paint, firstInside.X, firstInside.Y);
         int total = 0;
         for (int yy = 0; yy < height; yy++)
         {
             for (int xx = 0; xx < width; xx++)
             {
-                if (paint[yy, xx] != 0)
+                if (paint[yy][xx] != 0)
                 {
                     total++;
-                    continue;
                 }
-
-                int t = 0;
             }
         }
-
 
         return total.ToString();
     }
 
     protected override string SolvePart2(string[] input)
     {
-        return "";
+        //19838269529535
+        //19838269529535
+        int n = 1;
+        (long X, long Y) current = (0,0);
+
+        List<long> X = [0];
+        List<long> Y = [0];
+        //List<long> X = [0,4,4,0];
+        //List<long> Y = [0,0,4,4];
+        //List<long> X = [0,5,5,0];
+        //List<long> Y = [0,0,5,5];
+        //n = 4;
+        foreach (string line in input)
+        {
+            //var parts = line.Split(' ');
+            //int count = Convert.ToInt32(parts[2][2..7], 16);
+            //switch (parts[2][7])
+            //{
+            //    case '0': // up
+            //        current.Y -= count;
+            //        break;
+            //    case '1': // down
+            //        current.Y += count;
+            //        break;
+            //    case '2': // left
+            //        current.X -= count;
+            //        break;
+            //    case '3': // right
+            //        current.X += count;
+            //        break;
+            //    default:
+            //        throw new Exception("Unknown direction");
+            //};
+
+            var parts = line.Split(' ');
+            int count = int.Parse(parts[1]);
+            switch (parts[0][0])
+            {
+                case 'U': // up
+                    current.Y -= count;
+                    break;
+                case 'D': // down
+                    current.Y += count;
+                    break;
+                case 'L': // left
+                    current.X -= count;
+                    break;
+                case 'R': // right
+                    current.X += count;
+                    break;
+                default:
+                    throw new Exception("Unknown direction");
+            };
+
+            X.Add(current.X);
+            Y.Add(current.Y);
+            n++;
+        }
+
+        return ShoelaceArea(X.ToArray(), Y.ToArray(), n).ToString();
     }
 
     private struct PaintedCell
@@ -95,5 +159,39 @@ public class Day18 : AdventOfCodeDay
         public int Y { get; set; }
         public int Color { get; set; }
         public bool IsUpOrDown { get; set; }
+    }
+
+    private static void FloodFill(ref int[][] map, int x, int y)
+    {
+        Stack<(int x, int y)> stack = new();
+        stack.Push((x, y));
+
+        while (stack.Count != 0)
+        {
+            var c = stack.Pop();
+            if (c.x >= 0 && c.x < map[0].Length && c.y >= 0 && c.y < map.Length && map[c.y][c.x] == 0)
+            {
+                map[c.y][c.x] = -1;
+                stack.Push((c.x - 1, c.y));
+                stack.Push((c.x + 1, c.y));
+                stack.Push((c.x, c.y - 1));
+                stack.Push((c.x, c.y + 1));
+            }
+        }
+    }
+
+    private long ShoelaceArea(long[] X, long[] Y, int n)
+    {
+        long area = 0;
+
+        int j = n - 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            area += (X[j] + X[i]) * (Y[j] - Y[i]);
+            j = i;
+        }
+
+        return Math.Abs(area / 2);
     }
 }
